bf4b9b497bf4c57f3ca1bb4e382dbf8e54caa7e0
Gabriel Nadler <nadler.gabriel@gmail.com>
2015-12-31 14:11:48 +0100

    load all animelinks for an anime

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -2,37 +2,65 @@
 var request = require('./request');
 var cheerio = require('cheerio');
 var md5 = require('md5');
-var options = {
-    method: 'POST',
-    url: 'http://hi10anime.com/wp-login.php',
-    headers: {
-        'cache-control': 'no-cache',
-        'content-type': 'multipart/form-data; boundary=---011000010111000001101001',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'
-    },
-    formData: {
-        log: 'Archie',
-        pwd: 'p4$$word',
-        'wp-submit': 'login',
-        redirect_to: 'http://hi10anime.com/wp-admin/'
-    }
-};
 
-var optionsMain = {
-    method: 'GET',
-    url: 'http://hi10anime.com/projects/all-projects/',
-    headers: {
-        'cache-control': 'no-cache',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'
+var username = 'Archie',
+    password = 'p4$$word';
+
+var animelist = (() => {
+    var options = baseOptions();
+    options.url = 'http://hi10anime.com/projects/all-projects/';
+    return options;
+})();
+
+//{
+//    method: 'GET',
+//    url: 'http://hi10anime.com/projects/all-projects/',
+//    headers: {
+//        'cache-control': 'no-cache',
+//        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'
+//    }
+//};
+
+function baseOptions() {
+    return {
+        method: 'GET',
+        headers: {
+            'cache-control': 'no-cache',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'
+        }
     }
-};
+}
+
+function createAnimeRequestOptions(url) {
+    var options = baseOptions();
+    options.url = url;
+    return options;
+}
+
+function login(username, password) {
+    return request({
+        method: 'POST',
+        url: 'http://hi10anime.com/wp-login.php',
+        headers: {
+            'cache-control': 'no-cache',
+            'content-type': 'multipart/form-data; boundary=---011000010111000001101001',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'
+        },
+        formData: {
+            log: username,
+            pwd: password,
+            'wp-submit': 'login',
+            redirect_to: 'http://hi10anime.com/wp-admin/'
+        }
+    });
+}
 
 var Database = require('./database');
-function setup(loadFromNet) {
+function getAnimelist(loadFromNet) {
     if (loadFromNet) {
         return new Promise((resolve, reject) => {
-            request(options).then(function () {
-                return request(optionsMain)
+            login('Archie', 'p4$$word').then(function () {
+                return request(animelist)
             }).then(function (result) {
                 var $ = cheerio.load(result.body);
                 $('.projectsDiv > ul > li').each((i, ele)=> {
@@ -56,9 +84,37 @@ function setup(loadFromNet) {
             Database.load("data.json", (data) => resolve(data))
         })
     }
+}
 
+function getAnime(url) {
+    return login(username, password).then(() => {
+        return request(createAnimeRequestOptions(url))
+    }).then((animepage) => {
+        var $ = cheerio.load(animepage.body);
+        var results = [];
+        $('article').filter((i, ele) => {
+            return ele.attribs.class.indexOf('post') !== -1;
+        }).each((i, ele)=> {
+            $('a', ele).each((i, a) => {
+                results.push($(a).attr('href'));
+            });
+        });
+        return results.filter(link =>
+            typeof(link) !== 'undefined' &&
+            link.indexOf('http://hi10anime.com/') === -1 &&
+            link.indexOf('hi10anime') !== -1 &&
+            link.indexOf('mailto') === -1 &&
+            link.indexOf('torrent') === -1 &&
+            link.indexOf('forum') == -1
+        );
+        ////TODO: Parse page
+        //return 'no links found';
+    })
 }
 //
 
 
-module.exports = setup;
+module.exports = {
+    getAll: getAnimelist,
+    get: getAnime
+};
--- crawler/routes/anime.js
+++ crawler/routes/anime.js
@@ -4,7 +4,8 @@ var fuzzy = require('fuzzy');
 var router = express.Router();
 var animelist = [];
 var animeMap = {};
-require('../crawler')(false).then(anime => {
+var crawler = require('../crawler');
+crawler.getAll(false).then(anime => {
     animelist = anime;
     animelist.forEach((anime, index) => {
         animeMap[anime.id] = index;
@@ -15,7 +16,7 @@ require('../crawler')(false).then(anime => {
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    var start = 0, length = animelist.length > 20 ? 20 : animelist.length;
+    var start = 0, length = animelist.length > 40 ? 40 : animelist.length;
     if (req.query.start) {
         start = req.query.start;
     }
@@ -25,23 +26,51 @@ router.get('/', function (req, res, next) {
     res.json(animelist.slice(start, length));
 });
 
+function getAnime(id) {
+    return animelist[animeMap[id]];
+}
+
 router.get('/:id', function (req, res, next) {
         res.setHeader('Access-Control-Allow-Origin', '*');
-        res.json(animelist[animeMap[req.params.id]]);
+        res.json(getAnime(req.params.id));
     }
-)
-;
+);
+
+router.get('/load/:id', function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        crawler.get(getAnime(req.params.id).link).then((result) => {
+            res.json(result);
+        }).catch(function (err) {
+            res.status(500).send(err.message);
+        });
+        //.catch(res.status(500).send.bind(res));
+
+    }
+);
 
 router.post('/search', function (req, res, next) {
     var query = req.body.query;
+    var useFuzzy = req.body.fuzzy | false;
     var options = {
         extract: function (el) {
             return el.title;
         }
     };
-    var results = animelist.filter(anime => anime.title.toLowerCase().indexOf(query.toLowerCase()) !== -1);//fuzzy.filter(query, animelist, options);
+    var results;
+    if (useFuzzy) {
+        results = fuzzy.filter(query, animelist, options).map(el => el.original);
+    } else {
+        results = animelist.filter(anime => anime.title.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+    }
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.json(results);//.map(el => el.original));
+    //res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.json(results);//);
+})
+
+router.options('/search', function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 })
 
 module.exports = router;
